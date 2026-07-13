242ce42cb07711f83832b6c4ff15264ab6d9f1f6
wayanpratama38 <wayanpratama38@gmail.com>
2026-07-14 00:35:53 +0700

    feat: add diffrent path baseed on session context state

--- Aplikasi-Catatan-Submission-Kedua/src/App1.jsx
+++ Aplikasi-Catatan-Submission-Kedua/src/App1.jsx
@@ -1,33 +1,32 @@
-import React from "react";
-import { Routes, Route, useSearchParams } from "react-router-dom";
-import {
-  addNote,
-  archiveNote,
-  deleteNote,
-  getActiveNotes,
-  getArchivedNotes,
-  unarchiveNote,
-} from "./utils/network-data";
+import { Routes, Route, Navigate } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import DetailNotePage from "./pages/DetailNotePage";
 import ArchivePage from "./pages/ArchivePage";
 import NavigationBar from "./components/NavigationBar";
-import AddNotePageWrapper from "./pages/AddNotePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import useNote from "./hooks/useNote";
+import AddNotePage from "./pages/AddNotePage";
+import { useSession } from "./contexts/SessionContext";
 
 export default function AppWrapper() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const keyword = searchParams.get("title") || "";
-  const [archivedNotes, setArchivedNotes, isArchivedLoading] =
-    useNote(getArchivedNotes);
-  const [activeNotes, setActiveNotes, isActiveLoading] =
-    useNote(getActiveNotes);
+  const { user } = useSession();
 
-  const onChangeSearchParams = (keyword) => {
-    return setSearchParams({ title: keyword });
-  };
+  if (user === null) {
+    return (
+      <div className="app-container">
+        <header>
+          <NavigationBar />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
 
   return (
     <div className="app-container">
@@ -36,50 +35,12 @@ export default function AppWrapper() {
       </header>
       <main>
         <Routes>
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/"
-            element={
-              isActiveLoading ? (
-                <p>Memuat Catatan Aktif</p>
-              ) : (
-                <MainPage
-                  notes={activeNotes}
-                  onSearch={onChangeSearchParams}
-                  keyword={keyword}
-                />
-              )
-            }
-          />
-          <Route
-            path="/archives"
-            element={
-              isArchivedLoading ? (
-                <p>Memuat Catatan Arsip</p>
-              ) : (
-                <ArchivePage
-                  archivedNotes={archivedNotes}
-                  onSearch={onChangeSearchParams}
-                  keyword={keyword}
-                />
-              )
-            }
-          />
-          {/* <Route
-            path="/:id"
-            element={
-              <DetailNotePage
-                onDelete={this.onDeleteHandler}
-                onArchive={this.onArchiveHandler}
-                onUnarchive={this.onUnarchiveHandler}
-              />
-            }
-          />*/}
-          {/* <Route
-            path="/new"
-            element={<AddNotePageWrapper onAdd={this.onAddHandler} />}
-          />*/}
+          <Route path="/" element={<MainPage />} />
+          <Route path="/archives" element={<ArchivePage />} />
+          <Route path="/:id" element={<DetailNotePage />} />
+          <Route path="/new" element={<AddNotePage />} />
+          <Route path="/login" element={<Navigate to="/" replace />} />
+          <Route path="/register" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
     </div>
