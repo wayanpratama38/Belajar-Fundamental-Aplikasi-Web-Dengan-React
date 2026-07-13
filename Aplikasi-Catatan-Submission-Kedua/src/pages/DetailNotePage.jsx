b2acc5f36c6f64e0abf1956889b5ae20fb6dc34e
wayanpratama38 <wayanpratama38@gmail.com>
2026-07-13 23:36:11 +0700

    refactor: refactor how the DetailNotePage component to having own state and handling

--- Aplikasi-Catatan-Submission-Kedua/src/pages/DetailNotePage.jsx
+++ Aplikasi-Catatan-Submission-Kedua/src/pages/DetailNotePage.jsx
@@ -1,71 +1,61 @@
 import NotFoundPage from "./NotFoundPage";
 import { showFormattedDate } from "../utils";
 import { useParams, useNavigate } from "react-router-dom";
-import { getNote } from "../utils/local-data";
+import {
+  archiveNote,
+  deleteNote,
+  getNote,
+  unarchiveNote,
+} from "../utils/network-data";
 import Button from "../components/Button";
 import ArchiveIconDown from "../assets/archive-down.svg";
 import DeleteIcon from "../assets/delete.svg";
 import ArchiveIconUp from "../assets/archive-up.svg";
+import useNote from "../hooks/useNote";
 
-export default function DetailNotePage({ onDelete, onArchive, onUnarchive }) {
+export default function DetailNotePage() {
   const { id } = useParams();
-  const note = getNote(id);
-  const navigation = useNavigate();
+  const [note, _, isNoteLoading] = useNote(() => getNote(id));
+  const navigate = useNavigate();
+
   if (!note) {
     return <NotFoundPage />;
   }
-  function deleteNoteHandler() {
-    onDelete(id);
-    navigation("/");
-  }
 
-  function archiveNoteHandler() {
-    onArchive(id);
-    navigation("/");
-  }
-
-  function unarchiveNoteHandler() {
-    onUnarchive(id);
-    navigation("/");
-  }
+  const actionHandler = async (apiFunction) => {
+    const { error } = await apiFunction(id);
+    if (!error) navigate("/");
+  };
 
   return (
     <div className="detail-page">
-      <h1 className="detail-page__title">{note.title}</h1>
-      <p className="detail-page__createdAt">
-        {showFormattedDate(note.createdAt)}
-      </p>
-      <p className="detail-page__body">{note.body}</p>
-      <div className="detail-page__action">
-        <Button
-          type="submit"
-          image={note.archived ? ArchiveIconUp : ArchiveIconDown}
-          altImage={"Unarchive Icon"}
-          onClick={note.archived ? unarchiveNoteHandler : archiveNoteHandler}
-        />
-
-        {/* {note.archived ? (
-          <Button
-            type="submit"
-            image={ArchiveIconUp}
-            altImage={"Unarchive Icon"}
-            onClick={unarchiveNoteHandler}
-          />
-        ) : (
-          <Button
-            type="submit"
-            image={ArchiveIconDown}
-            altImage={"Archive Icon"}
-            onClick={archiveNoteHandler}
-          />
-        )} */}
-        <Button
-          type="submit"
-          image={DeleteIcon}
-          altImage={"Delete Icon"}
-          onClick={deleteNoteHandler}
-        />
-      </div>
+      {isNoteLoading ? (
+        <p>Memuat Detail Catatan</p>
+      ) : (
+        <>
+          <h1 className="detail-page__title">{note.title}</h1>
+          <p className="detail-page__createdAt">
+            {showFormattedDate(note.createdAt)}
+          </p>
+          <p className="detail-page__body">{note.body}</p>
+          <div className="detail-page__action">
+            <Button
+              type="submit"
+              image={note.archived ? ArchiveIconUp : ArchiveIconDown}
+              altImage={"Unarchive Icon"}
+              onClick={() =>
+                actionHandler(note.archived ? unarchiveNote : archiveNote)
+              }
+            />
+            <Button
+              type="submit"
+              image={DeleteIcon}
+              altImage={"Delete Icon"}
+              onClick={() => actionHandler(deleteNote)}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
