d8ddda8daa58dc4b3e0edaabe1c58de799ae6832
wayanpratama38 <wayanpratama38@gmail.com>
2026-07-14 05:43:11 +0700

    feat: implementing useLocale, adding new button for toggle theme and language

--- Aplikasi-Catatan-Submission-Kedua/src/components/NavigationBar.jsx
+++ Aplikasi-Catatan-Submission-Kedua/src/components/NavigationBar.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import { useSession } from "../contexts/SessionContext";
 import { useLocale } from "../contexts/LocaleContext";
+import { useTheme } from "../contexts/ThemeContext";
+import sun from "../assets/sun.svg";
+import moon from "../assets/moon.svg";
+
 export default function NavigationBar() {
   const { user, onLogout } = useSession();
-
+  const { theme, toggleTheme } = useTheme();
   const { text, toggleLocale } = useLocale();
 
   if (user === null) {
@@ -14,14 +18,17 @@ export default function NavigationBar() {
         </h1>
         <nav className="navigation">
           <ul>
-            <li>
-              <button onClick={toggleLocale}>{text.toggleLanguage}</button>
+            <li className="toggle-locale">
+              <button onClick={toggleLocale}>{ text.toggleLanguage}</button>
             </li>
-            <li>
-              <Link to="/register">{text.register}</Link>
+            <li className="toggle-theme">
+              <button onClick={toggleTheme}>{ theme === "dark" ? <img src={sun} alt="Sun" /> : <img src={moon} alt="Moon" /> }</button>
             </li>
-            <li>
+            <li className="input-login">
               <Link to="/login">{text.login}</Link>
+            </li>
+            <li className="input-register">
+              <Link to="/register">{text.register}</Link>
             </li>
           </ul>
         </nav>
@@ -39,11 +46,14 @@ export default function NavigationBar() {
           <li>
             <Link to="/archives">{text.archive}</Link>
           </li>
-          <li>
-            <button onClick={toggleLocale}>{text.toggleLanguage}</button>
+          <li className="toggle-locale">
+            <button onClick={toggleLocale}>{ text.toggleLanguage}</button>
           </li>
-          <li>
-            <button onClick={onLogout}>{text.logout}</button>
+          <li className="toggle-theme">
+            <button onClick={toggleTheme}>{ theme === "dark" ? <img src={sun} alt="Sun" /> : <img src={moon} alt="Moon" /> }</button>
+          </li>
+          <li className="button-logout">
+            <button className="button-logout" onClick={onLogout} >{text.logout}</button>
           </li>
         </ul>
       </nav>
