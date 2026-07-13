268ea9044e2cd4d2dd894c73ad76aa02adcc739d
wayanpratama38 <wayanpratama38@gmail.com>
2026-07-13 20:40:18 +0700

    feat: implementing sessionContext inside handleSubmit function

--- Aplikasi-Catatan-Submission-Kedua/src/pages/LoginPage.jsx
+++ Aplikasi-Catatan-Submission-Kedua/src/pages/LoginPage.jsx
@@ -1,28 +1,25 @@
 
 import React from 'react';
 import useInput from '../hooks/useInput';
-import { login, putAccessToken } from '../utils/network-data'
+import { useSession } from '../contexts/SessionContext';
+
 
 
 export default function LoginPage() {
   const [email, setEmail] = useInput('');
   const [password, setPassword] = useInput('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const result = await login({ email, password });
-    if (result.error) {
-      alert(result.message);
-    } else {
-      putAccessToken(email);
-      alert('Login successful');
-    }
+  const { onLogin } = useSession();
 
-  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    await onLogin({ email, password });
+  }
 
   return (
     <section className='login-page' >
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={submitHandler}>
         <input type='email' value={email} placeholder='email@gmail.com' onChange={setEmail} />
         <input type='password' value={password} placeholder='password' onChange={setPassword} />
         <button type='submit'>Login</button>
