78d61f6eb88fe9d73fa633e3fbc2f34e82fbeb3c
wayanpratama38 <wayanpratama38@gmail.com>
2026-07-13 21:51:07 +0700

    feat: remove class component and solely using function component

--- Aplikasi-Catatan-Submission-Kedua/src/App1.jsx
+++ Aplikasi-Catatan-Submission-Kedua/src/App1.jsx
@@ -1,15 +1,87 @@
-import React from 'react';
-import { Routes, Route, useSearchParams } from 'react-router-dom';
-import { SessionProvider } from './contexts/SessionContext';
+import React from "react";
+import { Routes, Route, useSearchParams } from "react-router-dom";
+import {
+  addNote,
+  archiveNote,
+  deleteNote,
+  getActiveNotes,
+  getArchivedNotes,
+  unarchiveNote,
+} from "./utils/network-data";
+import MainPage from "./pages/MainPage";
+import DetailNotePage from "./pages/DetailNotePage";
+import ArchivePage from "./pages/ArchivePage";
+import NavigationBar from "./components/NavigationBar";
+import AddNotePageWrapper from "./pages/AddNotePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import useNote from "./hooks/useNote";
 
+export default function AppWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const keyword = searchParams.get("title") || "";
+  const [archivedNotes, setArchivedNotes, isArchivedLoading] =
+    useNote(getArchivedNotes);
+  const [activeNotes, setActiveNotes, isActiveLoading] =
+    useNote(getActiveNotes);
 
-export default function App1() {
-  const [session, setSession] = useState(null);
-
+  const onChangeSearchParams = (keyword) => {
+    return setSearchParams({ title: keyword });
+  };
 
   return (
-    <SessionProvider value={{session, setSession}}>
-
-    </SessionProvider>
+    <div className="app-container">
+      <header>
+        <NavigationBar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/"
+            element={
+              isActiveLoading ? (
+                <p>Memuat Catatan Aktif</p>
+              ) : (
+                <MainPage
+                  notes={activeNotes}
+                  onSearch={onChangeSearchParams}
+                  keyword={keyword}
+                />
+              )
+            }
+          />
+          <Route
+            path="/archives"
+            element={
+              isArchivedLoading ? (
+                <p>Memuat Catatan Arsip</p>
+              ) : (
+                <ArchivePage
+                  archivedNotes={archivedNotes}
+                  onSearch={onChangeSearchParams}
+                  keyword={keyword}
+                />
+              )
+            }
+          />
+          {/* <Route
+            path="/:id"
+            element={
+              <DetailNotePage
+                onDelete={this.onDeleteHandler}
+                onArchive={this.onArchiveHandler}
+                onUnarchive={this.onUnarchiveHandler}
+              />
+            }
+          />*/}
+          {/* <Route
+            path="/new"
+            element={<AddNotePageWrapper onAdd={this.onAddHandler} />}
+          />*/}
+        </Routes>
+      </main>
+    </div>
   );
 }
--- Aplikasi-Catatan-Submission-Kedua/src/index.jsx
+++ Aplikasi-Catatan-Submission-Kedua/src/index.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
-import './styles/style.css';
-import AppWrapper from './App';
-import { SessionProvider } from './contexts/SessionContext';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/style.css";
+// import AppWrapper from './App';
+import AppWrapper from "./App1";
+import { SessionProvider } from "./contexts/SessionContext";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <SessionProvider>
       <AppWrapper />
     </SessionProvider>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
