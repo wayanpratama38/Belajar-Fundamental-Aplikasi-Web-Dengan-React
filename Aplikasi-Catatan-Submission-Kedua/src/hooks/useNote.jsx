d1dff95a995c5f7aa6c6d6c8b3da53b633d7a987
wayanpratama38 <wayanpratama38@gmail.com>
2026-07-13 21:49:56 +0700

    feat: create custom hooks on handling notes (active or archived)

---
+++ Aplikasi-Catatan-Submission-Kedua/src/hooks/useNote.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function useNote(fetchFunction) {
+  const [notes, setNotes] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  // UseEffect untuk fetch function
+  React.useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const { error, data } = await fetchFunction();
+
+      if (!error && data) {
+        setNotes(data);
+      }
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return [notes, setNotes, loading];
+}
