22f84afcf714abdb25004fa14846e28097a59ab7
wayanpratama38 <wayanpratama38@gmail.com>
2026-07-14 00:34:36 +0700

    refactor: refactor from class component into functional componeent

--- Aplikasi-Catatan-Submission-Kedua/src/pages/AddNotePage.jsx
+++ Aplikasi-Catatan-Submission-Kedua/src/pages/AddNotePage.jsx
@@ -1,69 +1,46 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import DoneIcon from "../assets/done.svg";
+import useInput from "../hooks/useInput";
+import { addNote } from "../utils/network-data";
 
-export default function AddNotePageWrapper({ onAdd }) {
+export default function AddNotePage() {
+  const [title, handleTitleChange] = useInput("");
+  const [body, handleBodyChange] = useInput("");
   const navigate = useNavigate();
-  return <AddNotePage navigate={navigate} onAdd={onAdd} />;
-}
 
-class AddNotePage extends React.Component {
-  constructor(props) {
-    super(props);
+  const handleAddButton = async () => {
+    const { error, data } = await addNote({ title, body });
 
-    this.state = {
-      title: "",
-      body: "",
-    };
+    if (!error && data) {
+      navigate("/");
+    }
+  };
 
-    this.onTitleChangeHandler = this.onTitleChangeHandler.bind(this);
-    this.onBodyChangeHandler = this.onBodyChangeHandler.bind(this);
-    this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
-  }
-
-  onTitleChangeHandler(event) {
-    this.setState({ title: event.target.value });
-  }
-
-  onBodyChangeHandler(event) {
-    this.setState({ body: event.target.value });
-  }
-
-  onAddNoteHandler() {
-    this.props.onAdd({
-      title: this.state.title,
-      body: this.state.body,
-    });
-    this.props.navigate("/");
-  }
-
-  render() {
-    return (
-      <section className="add-new-page">
-        <div className="add-new-page__input">
-          <input
-            className="add-new-page__input__title"
-            placeholder="Note Title"
-            value={this.state.title}
-            onChange={this.onTitleChangeHandler}
-          />
-          <textarea
-            className="add-new-page__input__body"
-            placeholder="Body"
-            value={this.state.body}
-            onChange={this.onBodyChangeHandler}
-          />
-        </div>
-        <div className="add-new-page__action">
-          <Button
-            type="submit"
-            image={DoneIcon}
-            altImage={"Add Icon"}
-            onClick={this.onAddNoteHandler}
-          />
-        </div>
-      </section>
-    );
-  }
+  return (
+    <section className="add-new-page">
+      <div className="add-new-page__input">
+        <input
+          className="add-new-page__input__title"
+          placeholder="Note Title"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <textarea
+          className="add-new-page__input__body"
+          placeholder="Body"
+          value={body}
+          onChange={handleBodyChange}
+        />
+      </div>
+      <div className="add-new-page__action">
+        <Button
+          type="submit"
+          image={DoneIcon}
+          altImage={"Add Icon"}
+          onClick={handleAddButton}
+        />
+      </div>
+    </section>
+  );
 }
