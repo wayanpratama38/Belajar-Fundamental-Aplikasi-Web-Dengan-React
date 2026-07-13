0ac12b0cb7ca51f3db9257f80323a066b06aae36
wayanpratama38 <wayanpratama38@gmail.com>
2026-07-13 23:35:22 +0700

    refactor: refactor how the ArchivePage component tot be having own state and handling

--- Aplikasi-Catatan-Submission-Kedua/src/pages/ArchivePage.jsx
+++ Aplikasi-Catatan-Submission-Kedua/src/pages/ArchivePage.jsx
@@ -1,16 +1,26 @@
 import SearchBar from "../components/SearchBar";
 import NoteList from "../components/NoteList";
+import { getArchivedNotes } from "../utils/network-data";
+import useSearch from "../hooks/useSearch";
+import useNote from "../hooks/useNote";
 
-export default function ArchivePage({ archivedNotes, keyword, onSearch }) {
+export default function ArchivePage() {
+  const [archivedNotes, _, isArchivedLoading] = useNote(getArchivedNotes);
+  const [keyword, setKeyword] = useSearch("title");
+
   return (
     <section className="archivepage">
       <SearchBar
         text={"Catatan Arsip"}
         placeholder={"Silahkan isi dengan nama catatan arsip"}
         keyword={keyword}
-        onSearch={onSearch}
+        onSearch={setKeyword}
       />
-      <NoteList notes={archivedNotes} />
+      {isArchivedLoading ? (
+        <p>Memuat Catatan Arsip</p>
+      ) : (
+        <NoteList notes={archivedNotes} />
+      )}
     </section>
   );
 }
