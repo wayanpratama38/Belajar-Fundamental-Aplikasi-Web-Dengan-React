b314d5845e1fd9d77d727d8e54b6d8cd2008c289
wayanpratama38 <wayanpratama38@gmail.com>
2026-07-14 00:35:21 +0700

    feat: implement logout handler and create different navigation based on session context

--- Aplikasi-Catatan-Submission-Kedua/src/components/NavigationBar.jsx
+++ Aplikasi-Catatan-Submission-Kedua/src/components/NavigationBar.jsx
@@ -1,5 +1,28 @@
 import { Link } from "react-router-dom";
+import { useSession } from "../contexts/SessionContext";
 export default function NavigationBar() {
+  const { user, onLogout } = useSession();
+
+  if (user === null) {
+    return (
+      <>
+        <h1>
+          <Link>Aplikasi Catatan</Link>
+        </h1>
+        <nav className="navigation">
+          <ul>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">login</Link>
+            </li>
+          </ul>
+        </nav>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>
@@ -9,6 +32,9 @@ export default function NavigationBar() {
         <ul>
           <li>
             <Link to="/archives">Archive</Link>
+          </li>
+          <li>
+            <button onClick={onLogout}>Logout</button>
           </li>
         </ul>
       </nav>
